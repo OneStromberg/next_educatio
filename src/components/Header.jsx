574bb17fc787a700fb267e156cee8d71632cf95d
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-06-09 23:27:56 +0400

    added burger-menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import Link from 'next/link';
-import { AppBar, Toolbar, Typography, IconButton, Button } from '@mui/material';
-import { Language, Facebook, Instagram } from '@mui/icons-material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Hidden, // Добавлено
+} from '@mui/material';
+import { Language, Facebook, Instagram, Menu } from '@mui/icons-material';
 import { styled } from '@mui/system';
 
 const TransparentAppBar = styled(AppBar)`
@@ -25,36 +37,78 @@ const Logo = styled(Typography)`
 `;
 
 const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <TransparentAppBar position="static">
       <Toolbar>
         <Logo variant="h6" component="div">
           My Website
         </Logo>
-        <Button color="inherit">
-          <StyledLink href="/about">About Us</StyledLink>
-        </Button>
-        <Button color="inherit">
-          <StyledLink href="/services">Services</StyledLink>
-        </Button>
-        <Button color="inherit">
-          <StyledLink href="/news">News</StyledLink>
-        </Button>
-        <Button color="inherit">
-          <StyledLink href="/calendar">Calendar</StyledLink>
-        </Button>
-        <Button color="inherit">
-          <StyledLink href="/contact">Contact</StyledLink>
-        </Button>
-        <StyledIconButton color="inherit">
-          <Language />
-        </StyledIconButton>
-        <StyledIconButton color="inherit" href="https://www.facebook.com">
-          <Facebook />
-        </StyledIconButton>
-        <StyledIconButton color="inherit" href="https://www.instagram.com">
-          <Instagram />
-        </StyledIconButton>
+        <Hidden mdUp> {/* Добавлено */}
+          <IconButton color="inherit" onClick={handleMenuToggle}>
+            <Menu />
+          </IconButton>
+        </Hidden>
+        <Hidden smDown> {/* Добавлено */}
+          <Button color="inherit">
+            <StyledLink href="/about">About Us</StyledLink>
+          </Button>
+          <Button color="inherit">
+            <StyledLink href="/services">Services</StyledLink>
+          </Button>
+          <Button color="inherit">
+            <StyledLink href="/news">News</StyledLink>
+          </Button>
+          <Button color="inherit">
+            <StyledLink href="/calendar">Calendar</StyledLink>
+          </Button>
+          <Button color="inherit">
+            <StyledLink href="/contact">Contact</StyledLink>
+          </Button>
+          <StyledIconButton color="inherit">
+            <Language />
+          </StyledIconButton>
+          <StyledIconButton color="inherit" href="https://www.facebook.com">
+            <Facebook />
+          </StyledIconButton>
+          <StyledIconButton color="inherit" href="https://www.instagram.com">
+            <Instagram />
+          </StyledIconButton>
+        </Hidden>
+        <Drawer anchor="right" open={isMenuOpen} onClose={handleMenuToggle}>
+          <List>
+            <ListItem button>
+              <ListItemText>
+                <StyledLink href="/about">About Us</StyledLink>
+              </ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemText>
+                <StyledLink href="/services">Services</StyledLink>
+              </ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemText>
+                <StyledLink href="/news">News</StyledLink>
+              </ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemText>
+                <StyledLink href="/calendar">Calendar</StyledLink>
+              </ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemText>
+                <StyledLink href="/contact">Contact</StyledLink>
+              </ListItemText>
+            </ListItem>
+          </List>
+        </Drawer>
       </Toolbar>
     </TransparentAppBar>
   );
